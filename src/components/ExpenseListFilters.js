06334966a44e1fd345dc7333fd2b9bf794d055bd
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-10-07 21:22:14 -0300

    components for listing and filtering expenses

---
+++ src/components/ExpenseListFilters.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { connect } from "react-redux";
+import { setTextFilter, sortByDate, sortByAmount } from "../actions/filters";
+
+const ExpenseListFilters = props => (
+  <div>
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={event => {
+        props.dispatch(setTextFilter(event.target.value));
+      }}
+    />
+    <select
+      value={props.filters.sortBy}
+      onChange={event => {
+        if (event.target.value === "date") {
+          props.dispatch(sortByDate());
+        } else if (event.target.value === "amount") {
+          props.dispatch(sortByAmount());
+        }
+      }}
+    >
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
+  </div>
+);
+
+const mapStateToProps = state => {
+  return {
+    filters: state.filters
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseListFilters);
