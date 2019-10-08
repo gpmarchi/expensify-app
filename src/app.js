38fa3e48b40af82b030eeb8c6294d54f8ae41979
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-10-07 21:14:22 -0300

    hooking up the redux store to react via provider

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -16,8 +16,14 @@ store.subscribe(() => {
   console.log(visibleExpenses);
   // console.log(store.getState());
 });
-store.dispatch(addExpense({ description: "Water bill", amount: 100 }));
-store.dispatch(addExpense({ description: "Gas bill", amount: 200 }));
-store.dispatch(setTextFilter("water"));
+store.dispatch(addExpense({ description: "Water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Gas bill", createdAt: 1000 }));
+store.dispatch(addExpense({ description: "Rent", amount: 109500 }));
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
