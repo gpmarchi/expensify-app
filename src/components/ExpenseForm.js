13457da66bdb565b7805a79ce8a480917c41cf2f
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-10-31 18:04:47 -0300

    restructuring and styling of all pages

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -61,38 +61,43 @@ export default class ExpenseForm extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="Description"
-            autoFocus
-            value={this.state.description}
-            onChange={this.handleDescriptionChange}
-          />
-          <input
-            type="text"
-            placeholder="Amount"
-            value={this.state.amount}
-            onChange={this.handleAmountChange}
-          />
-          <SingleDatePicker
-            date={this.state.createdAt}
-            onDateChange={this.handleDateChange}
-            focused={this.state.calendarFocused}
-            onFocusChange={this.handleFocusChange}
-            numberOfMonths={1}
-            isOutsideRange={() => false}
-          />
-          <textarea
-            placeholder="Add a note for your expenses (optional)"
-            value={this.state.note}
-            onChange={this.handleNoteChange}
-          />
-          <button type="submit">Add Expense</button>
-        </form>
-      </div>
+      <form className="form" onSubmit={this.handleSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
+        <input
+          type="text"
+          placeholder="Description"
+          autoFocus
+          className="text-input"
+          value={this.state.description}
+          onChange={this.handleDescriptionChange}
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          className="text-input"
+          value={this.state.amount}
+          onChange={this.handleAmountChange}
+        />
+        <SingleDatePicker
+          date={this.state.createdAt}
+          onDateChange={this.handleDateChange}
+          focused={this.state.calendarFocused}
+          onFocusChange={this.handleFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+        <textarea
+          placeholder="Add a note for your expenses (optional)"
+          className="text-area"
+          value={this.state.note}
+          onChange={this.handleNoteChange}
+        />
+        <div>
+          <button className="button " type="submit">
+            Save Expense
+          </button>
+        </div>
+      </form>
     );
   }
 }
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -38,39 +38,50 @@ export class ExpenseListFilters extends React.Component {
 
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          value={this.props.filters.text}
-          onChange={this.handleTextChange}
-        />
-        <select
-          value={this.props.filters.sortBy}
-          onChange={this.handleSortChange}
-        >
-          <option value="date">Date</option>
-          <option value="amount">Amount</option>
-        </select>
-        <DateRangePicker
-          startDate={this.props.filters.startDate}
-          startDateId={
-            this.props.filters.startDate
-              ? this.props.filters.startDate.toString()
-              : ""
-          }
-          endDate={this.props.filters.endDate}
-          endDateId={
-            this.props.filters.endDate
-              ? this.props.filters.endDate.toString()
-              : ""
-          }
-          onDatesChange={this.handleDatesChange}
-          focusedInput={this.state.calendarFocused}
-          onFocusChange={this.handleFocusChange}
-          showClearDates={true}
-          numberOfMonths={1}
-          isOutsideRange={() => false}
-        />
+      <div className="content-container">
+        <div className="input-group">
+          <div className="input-group__item">
+            <input
+              type="text"
+              className="text-input"
+              placeholder="Search expenses"
+              value={this.props.filters.text}
+              onChange={this.handleTextChange}
+            />
+          </div>
+          <div className="input-group__item">
+            <select
+              className="select"
+              value={this.props.filters.sortBy}
+              onChange={this.handleSortChange}
+            >
+              <option value="date">Date</option>
+              <option value="amount">Amount</option>
+            </select>
+          </div>
+          <div className="input-group__item">
+            <DateRangePicker
+              startDate={this.props.filters.startDate}
+              startDateId={
+                this.props.filters.startDate
+                  ? this.props.filters.startDate.toString()
+                  : ""
+              }
+              endDate={this.props.filters.endDate}
+              endDateId={
+                this.props.filters.endDate
+                  ? this.props.filters.endDate.toString()
+                  : ""
+              }
+              onDatesChange={this.handleDatesChange}
+              focusedInput={this.state.calendarFocused}
+              onFocusChange={this.handleFocusChange}
+              showClearDates={true}
+              numberOfMonths={1}
+              isOutsideRange={() => false}
+            />
+          </div>
+        </div>
       </div>
     );
   }
